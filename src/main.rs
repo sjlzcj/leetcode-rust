f186228b181bf8e82fd5e214084a734553dd94bd
宋金龙 <songjinlong@songjinlongdeMacBook-Pro.local>
2023-01-02 19:59:34 +0800

    two_sum

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod two_sum;
 fn main() {
-    let nums = vec![1, 2, 3, 4, 5];
+    let nums = vec![1, 2, 5, 4, 3];
     let results = two_sum::solution(nums, 8);
     for ele in results {
         println!("{}", ele);
--- src/two_sum.rs
+++ src/two_sum.rs
@@ -1,11 +1,15 @@
 pub fn solution(nums: Vec<i32>, target: i32) -> Vec<i32> {
     let mut seen = std::collections::HashMap::new();
-
     for (i, n) in nums.iter().enumerate() {
+        let ii = i as i32;
         if let Some(p) = seen.get(n) {
-            return vec![*p, i as i32];
+            if *p < ii {
+                return vec![*p, ii];
+            } else {
+                return vec![ii, *p];
+            }
         }
-        seen.insert(target - *n, i as i32);
+        seen.insert(target - *n, ii);
     }
     vec![]
 }
